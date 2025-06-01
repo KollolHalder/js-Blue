6c7bbc33be27aa876f420e09c1923926930efa24
KollolHalder <kollolhal1@gmail.com>
2025-06-01 10:13:52 +0600

    date

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -13,3 +13,14 @@ console.log(date.toString());
 console.log(date.toDateString());
 console.log(date.toTimeString());
 console.log(date.toISOString());
+console.log(typeof date);
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+
+// let createdDate = new Date(2025,6,1); // erokom patterene likhle month 0 theke shuru hoy.
+let createdDate2 = new Date("2025-06-1"); // Erokom kore likhle month a theke shuru hoy.
+
+let createdDate = new Date("6-1-2025");
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(createdDate2.getTime());
