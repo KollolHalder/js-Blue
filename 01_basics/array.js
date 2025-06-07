9eeb7d2ba2067db46bfa0322a1c13a252b082be9
KollolHalder <kollolhal1@gmail.com>
2025-06-07 20:37:12 +0600

    arrays

---
+++ 01_basics/array.js
@@ -0,0 +1,10 @@
+const array = [1, 2, 3, 4, 5, 6];
+
+const arr = array.slice(1, 4);
+const arr2 = array.splice(1, 4);
+
+console.log(arr);
+console.log(arr2);
+
+// slice e array change hoy na
+// splice e array change hoye jay argument[array.splice(1, 4)] onujai
